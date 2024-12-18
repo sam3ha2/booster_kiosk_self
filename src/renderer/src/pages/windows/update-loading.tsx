52e8242da00e6518ceb0c6cd402b7d93e21dc51e
follow <follow@boosteron.co.kr>
2024-12-18 23:52:03 +0900

    init

---
+++ src/renderer/src/pages/windows/update-loading.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import useSWR from 'swr'
+
+import logoImage from '@renderer/assets/electron.svg'
+import '@renderer/assets/update-loading.css'
+
+const UpdateLoadingPage = () => {
+  const { t } = useTranslation('translation', {
+    keyPrefix: 'renderer.updateLoading',
+  })
+
+  const [percent, setPercent] = useState(0)
+  const [text, setText] = useState(t('loading'))
+
+  const { data: status, mutate } = useSWR('update-status', window.electron.getUpdateStatus)
+
+  useEffect(() => {
+    window.electron.onChangeUpdateStatus(status => {
+      mutate(status, {
+        revalidate: false,
+      })
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!status) return
+
+    switch (status.event) {
+      case 'download-progress': {
+        if (percent !== 100) setPercent(Number(status.data.percent))
+        break
+      }
+
+      case 'update-downloaded': {
+        setText(t('autoRestart'))
+        break
+      }
+    }
+  }, [status])
+
+  return (
+    <div className="update-loading-window">
+      <img src={logoImage} alt="logo" />
+      <div className="text">{text}</div>
+      <progress value={20} max={100} />
+    </div>
+  )
+}
+
+export default UpdateLoadingPage
